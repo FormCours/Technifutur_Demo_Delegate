b6a9a874bc2f83296eddff28f5d78abf45196f49
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-01-25 13:47:46 +0100

    Ajoutez des fichiers projet.

---
+++ Demo_Delegate/Models/ElementList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Delegate.Models
+{
+    public class ElementList
+    {
+        private List<int> _InternalContent;
+
+
+        //public IEnumerable<int> Content
+        //{
+        //    // Fonctionnel, mais duplication des données en memoire
+        //    get { return new List<int>(_InternalContent); }
+        //}
+
+        //public IEnumerable<int> Content
+        //{
+        //    // Retour des données sous forme d'un yield return => OK
+        //    get
+        //    {
+        //        foreach (var item in _InternalContent)
+        //        {
+        //            yield return item;
+        //        }
+        //    }
+        //}
+
+        public IEnumerable<int> Content
+        {
+
+            get { return _InternalContent.Select(elem => elem); }
+        }
+
+        public ElementList()
+        {
+            _InternalContent = new List<int>();
+        }
+
+        public void Generate(int count)
+        {
+            Random rng = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                _InternalContent.Add(rng.Next(-100, 101));
+            }
+        }
+
+        public IEnumerable<int> GetEven()
+        {
+            foreach (int value in _InternalContent)
+            {
+                if(value % 2 == 0)
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        public IEnumerable<int> GetOdd()
+        {
+            foreach (int value in _InternalContent)
+            {
+                if (value % 2 != 0)
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        public IEnumerable<int> Filter(LeDelegateDeNico del)
+        {
+            foreach (int value in _InternalContent)
+            {
+                if (del(value))
+                {
+                    yield return value;
+                }
+            }
+        }
+    }
+
+    public delegate bool LeDelegateDeNico(int v);
+}
---
+++ Demo_Delegate/Program.cs
@@ -0,0 +1,72 @@
+﻿using Demo_Delegate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ElementList el = new ElementList();
+
+            Console.WriteLine("Generation et affichage des valeurs");
+            el.Generate(10);
+            foreach(int value in el.Content)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Les nombres paires");
+            IEnumerable<int> v1 = el.GetEven();
+            foreach (int value in v1)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Les nombres impaires");
+            IEnumerable<int> v2 = el.GetOdd();
+            foreach (int value in v2)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Les nombres multi de 3");
+            LeDelegateDeNico d1 = delegate (int nb)
+            {
+                return nb % 3 == 0;
+            };
+
+            IEnumerable<int> vd1 = el.Filter(d1);
+            foreach (int value in vd1)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Les nombres multi de 5");
+            LeDelegateDeNico d2 = (v) => v % 5 == 0;
+
+
+            IEnumerable<int> vd2 = el.Filter(d2);
+            foreach (int value in vd2)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
